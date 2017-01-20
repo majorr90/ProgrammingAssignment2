3da3c5d7c990e4de657e626d974de5e015e063e6
Matt <matt@matthewjorr.com>
2017-01-20 14:52:13 -0500

    Edited cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions cache information that can be returned later
 
-## Write a short comment describing this function
+## makeCacheMatrix() takes a vector and turns it into a square matrix. If it's already a square matrix,
+## then it stays the same. It stores the matrix in the function get(), and the default inverse of NULL in
+## the function getinverse(). Both of these functions simply return the stored value. When setinverse()
+## is called, it updates the value of the inverse i. These custom functions are stored in a list, from which
+## they can be called.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  x <- matrix(x, sqrt(length(x)), sqrt(length(x)))
+  get <- function() x
+  setinverse <- function(solve) i <<- solve
+  getinverse <- function() i
+  list(get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() requires from the list output from makeCacheMatrix(). Using the custom functions there, it
+## assigns i a value using the getinverse() in the list from makeCacheMatrix(). If the value of i is not
+## NULL then a message is returned saying "getting cached data" followed by the value of i, and the program is
+## stopped. If i does equal NULL, then matrix is retrieved using get() and then the inverse is calculated
+## using the built-in function solve(). cacheSolve() then calls the custom function setinverse() and updates the
+## cached value for the inverse before returning it and ending the program.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(CacheMatrix, ...) {
+  i <- CacheMatrix$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    i
+  }
+  i <- solve(CacheMatrix$get())
+  CacheMatrix$setinverse(i)
+  i
 }
